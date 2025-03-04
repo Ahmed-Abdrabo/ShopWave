9b98e98b749d7f672ba35c7fee56048d46007370
Ahmed-Abdrabo <am3991156@gmail.com>
2025-03-05 01:10:59 +0200

    Add Catalog module

---
+++ src/BuildingBlocks/BuildingBlocks/Exceptions/BadRequestException.cs
@@ -0,0 +1,15 @@
+﻿namespace BuildingBlocks.Exceptions;
+
+public class BadRequestException : Exception
+{
+    public BadRequestException(string massage):base(massage)
+    {
+        
+    }
+    public BadRequestException(string message, string details) : base(message)
+    {
+        Details = details;
+    }
+
+    public string? Details { get; }
+}
