83b94c0b778be0bbdb3b6d62e50ac3a5db4dcb71
Ahmed-Abdrabo <am3991156@gmail.com>
2025-03-13 03:37:41 +0200

    Add Order module

---
+++ src/BuildingBlocks/BuildingBlocks/Pagination/PaginatedResult.cs
@@ -0,0 +1,10 @@
+﻿namespace BuildingBlocks.Pagination;
+
+public class PaginatedResult<TEntity>
+    (int pageIndex, int pageSize, long count, IEnumerable<TEntity> data) where TEntity : class
+{
+    public int PageSize { get; } = pageSize;
+    public int PageIndex { get; } = pageIndex;
+    public long Count { get; } = count;
+    public IEnumerable<TEntity> Data { get; } = data; 
+}
